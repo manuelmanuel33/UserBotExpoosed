d6dae6e9263c01271964cad180c336a2b99958c0
manuelmanuel33 <66557381+manuelmanuel33@users.noreply.github.com>
2020-06-07 16:02:57 +0200

    Update _helper.py

--- userbot/plugin/_helper.py
+++ userbot/plugin/_helper.py
@@ -8,7 +8,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "✨ " + i + "\n"
+                string += "💦 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -37,8 +37,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \n Provided by @Hack12R\n
-`Userbot Helper to reveal all the commands`"""
+            help_string = """Userbot Helper.. \n FATTO DA @EXPOOSED\n
+`Aiuti per i comandi dell'Userbot`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
